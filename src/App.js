0afbc7e5ec910639fe9f5961ce08e58cb5399c3a
Ariful Islam <arifkhagoria2015@gmail.com>
2022-01-15 00:40:17 +0600

    Code added

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import DailyReport from './Components/DailyReport';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <DailyReport/>
+      </div>
+    );
+  }
+}
+
+export default App;
